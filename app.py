5bfede6c4d52b206c67ac8fb7666d777421bbfbb
Rafael Bruno <rafarbop@gmail.com>
2021-07-29 14:14:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,20 @@ import streamlit as st
 import pandas as pd
 import tabula as tb
 
-
+# Ajust layout and visual of streamlit
+st.set_page_config(
+    page_title="PDF to XML-SIAFI",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
 
 # Title and Description of Application
 st.markdown("# Converter PDF para Arquivo XML do SIAFI")
 st.markdown("---")
 
+st.sidebar.title('Rafa')
+
+fileUploaded = st.sidebar.file_uploader(
+    "PDF",
+    type="pdf",
+)
