c94595693b89d194b4a0acb74a53b1e51bd20927
Rafael Bruno <rafarbop@gmail.com>
2021-07-29 14:14:25 -0300

    Started App

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import tabula as tb
+
+
+
+# Title and Description of Application
+st.markdown("# Converter PDF para Arquivo XML do SIAFI")
+st.markdown("---")
+
