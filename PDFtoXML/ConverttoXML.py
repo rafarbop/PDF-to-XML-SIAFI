da273cf120a71384c5ce7f059c5b785e2dc9e551
Rafael Bruno <rafarbop@gmail.com>
2021-10-08 13:28:24 -0300

    Funcionalidades Básicas OK

--- PDFtoXML/ConverttoXML.py
+++ PDFtoXML/ConverttoXML.py
@@ -56,79 +56,90 @@ class convertXML:
         dadosDH['codUgEmit'] = self.codUgEmit
         dadosDH['anoDH'] = self.anoDH
         dadosDH['codTipoDH'] = self.codTipoDH
-        dadosDH['dadosBasicos'] = {
-            "dtEmis": self.dtEmis,
-            "dtVenc": self.dtVenc,
-            "codUgPgto": self.codUgPgto,
-            "vlr": self.vlr,
-            "txtObser": self.txtObser,
-            "txtProcesso": self.txtProcesso,
-            "dtAteste": self.dtAteste,
-            "codCredorDevedor": self.codCredorDevedor,
-            "dtPgtoReceb": self.dtPgtoReceb,
-            "docOrigem": {
-                "codIdentEmit": self.codIdentEmit,
-                "dtEmis": self.dtEmisDocOrigem,
-                "numDocOrigem": self.numDocOrigem,
-                "vlr": self.vlr,
-            },
-        }
-        dadosDH['pco'] = {
-            "numSeqItem": self.numSeqItemPco,
-            "codSit": self.codSit,
-            "codUgEmpe": self.codUgEmpe,
-            "pcoItem": {
-                "numSeqItem": self.numSeqItemPco,
-                "numEmpe": self.numEmpe,
-                "codSubItemEmpe": self.codSubItemEmpe,
-                "vlr": self.vlr,
-                "numClassA": self.numClassA,
-            },
-        }
-        dadosDH['centroCusto'] = {
-            "numSeqItem": self.numSeqItemCusto,
-            "codCentroCusto": self.codCentroCusto,
-            "mesReferencia": self.mesReferencia,
-            "anoReferencia": self.anoReferencia,
-            "codUgBenef": self.codUgBenef,
-            "relPcoItem": {
-                "numSeqPai": self.numSeqPai,
-                "numSeqItem": self.numSeqItemCusto,
-                "vlr": self.vlr,
-            },
-        }
-        dadosDH['dadosPgto'] = {
-            "codCredorDevedor": self.codCredorDevedor,
-            "vlr": self.vlr,
-            "predoc": {
-                "txtObser": self.txtObser,
-                "predocOB": {
-                    "codTipoOB": self.codTipoOB,
-                    "codCredorDevedor": self.codCredorDevedor,
-                    "numDomiBancFavo": {
-                        "banco": self.banco,
-                        "agencia": self.agencia,
-                        "conta": self.conta,
-                    },
-                    "numDomiBancPgto": {"conta": self.contaGoverno},
-                },
-            },
-        }
+        
+        dadosDH_dadosBasicos = OrderedDict()
+        dadosDH_dadosBasicos["dtEmis"] = self.dtEmis
+        dadosDH_dadosBasicos["dtVenc"] = self.dtVenc
+        dadosDH_dadosBasicos["codUgPgto"] = self.codUgPgto
+        dadosDH_dadosBasicos["vlr"] = self.vlr
+        dadosDH_dadosBasicos["txtObser"] = self.txtObser
+        dadosDH_dadosBasicos["txtProcesso"] = self.txtProcesso
+        dadosDH_dadosBasicos["dtAteste"] = self.dtAteste
+        dadosDH_dadosBasicos["codCredorDevedor"] = self.codCredorDevedor
+        dadosDH_dadosBasicos["dtPgtoReceb"] = self.dtPgtoReceb
+        dadosDH_dadosBasicos_docOrigem = OrderedDict()
+        dadosDH_dadosBasicos_docOrigem["codIdentEmit"] = self.codIdentEmit
+        dadosDH_dadosBasicos_docOrigem["dtEmis"] = self.dtEmisDocOrigem
+        dadosDH_dadosBasicos_docOrigem["numDocOrigem"] = self.numDocOrigem
+        dadosDH_dadosBasicos_docOrigem["vlr"] = self.vlr
+        dadosDH_dadosBasicos["docOrigem"] = dadosDH_dadosBasicos_docOrigem
+        dadosDH['dadosBasicos'] = dadosDH_dadosBasicos
+        
+        dadosDH_pco = OrderedDict()
+        dadosDH_pco["numSeqItem"] = self.numSeqItemPco
+        dadosDH_pco["codSit"] = self.codSit
+        dadosDH_pco["codUgEmpe"] = self.codUgEmpe
+        dadosDH_pco_pcoItem = OrderedDict()
+        dadosDH_pco_pcoItem["numSeqItem"] = self.numSeqItemPco
+        dadosDH_pco_pcoItem["numEmpe"] = self.numEmpe,
+        dadosDH_pco_pcoItem["codSubItemEmpe"] = self.codSubItemEmpe
+        dadosDH_pco_pcoItem["vlr"] = self.vlr
+        dadosDH_pco_pcoItem["numClassA"] = self.numClassA
+        dadosDH_pco["pcoItem"] = dadosDH_pco_pcoItem
+        dadosDH['pco'] = dadosDH_pco
+
+        dadosDH_centroCusto = OrderedDict()
+        dadosDH_centroCusto["numSeqItem"] = self.numSeqItemCusto
+        dadosDH_centroCusto["codCentroCusto"] = self.codCentroCusto
+        dadosDH_centroCusto["mesReferencia"] = self.mesReferencia
+        dadosDH_centroCusto["anoReferencia"] = self.anoReferencia
+        dadosDH_centroCusto["codUgBenef"] = self.codUgBenef
+        dadosDH_centroCusto_relPcoItem = OrderedDict()
+        dadosDH_centroCusto_relPcoItem["numSeqPai"] = self.numSeqPai
+        dadosDH_centroCusto_relPcoItem["numSeqItem"] = self.numSeqItemCusto
+        dadosDH_centroCusto_relPcoItem["vlr"] = self.vlr
+        dadosDH_centroCusto["relPcoItem"] = dadosDH_centroCusto_relPcoItem
+        dadosDH['centroCusto'] = dadosDH_centroCusto
+
+        dadosDH_dadosPgto = OrderedDict()
+        dadosDH_dadosPgto["codCredorDevedor"] = self.codCredorDevedor
+        dadosDH_dadosPgto["vlr"] = self.vlr
+        dadosDH_dadosPgto_predoc = OrderedDict()
+        dadosDH_dadosPgto_predoc["txtObser"] = self.txtObser
+        dadosDH_dadosPgto_predoc_predocOB = OrderedDict()
+        dadosDH_dadosPgto_predoc_predocOB["codTipoOB"] = self.codTipoOB
+        dadosDH_dadosPgto_predoc_predocOB["codCredorDevedor"] = self.codCredorDevedor
+        dadosDH_dadosPgto_predoc_predocOB_numDomiBancFavo = OrderedDict()
+        dadosDH_dadosPgto_predoc_predocOB_numDomiBancFavo["banco"] = self.banco
+        dadosDH_dadosPgto_predoc_predocOB_numDomiBancFavo["agencia"] = self.agencia
+        dadosDH_dadosPgto_predoc_predocOB_numDomiBancFavo["conta"] = self.conta
+        dadosDH_dadosPgto_predoc_predocOB_numDomiBancPgto = OrderedDict()
+        dadosDH_dadosPgto_predoc_predocOB_numDomiBancPgto["conta"] = self.contaGoverno
+        dadosDH_dadosPgto_predoc_predocOB["numDomiBancFavo"] = dadosDH_dadosPgto_predoc_predocOB_numDomiBancFavo
+        dadosDH_dadosPgto_predoc_predocOB["numDomiBancPgto"] = dadosDH_dadosPgto_predoc_predocOB_numDomiBancPgto
+        dadosDH_dadosPgto_predoc_predocOB["txtProcesso"] = self.txtProcesso
+        dadosDH_dadosPgto_predoc["predocOB"] = dadosDH_dadosPgto_predoc_predocOB
+        dadosDH_dadosPgto["predoc"] = dadosDH_dadosPgto_predoc
+        dadosDH['dadosPgto'] = dadosDH_dadosPgto
+        
         lista_dados['ns2:CprDhCadastrar'] = dadosDH
 
         return lista_dados
 
 
-def receberXML(lista_de_detalhes):
+def receberXML(lista_de_detalhes,dados_header):
     dados_arquivos = OrderedDict()
-    dados_arquivos['sb:header'] = {
-                "sb:codigoLayout": "DH001",
-                "sb:dataGeracao": "01/12/2020",
-                "sb:sequencialGeracao": "2",
-                "sb:anoReferencia": "2020",
-                "sb:ugResponsavel": "158953",
-                "sb:cpfResponsavel": "60032673302",
-            }
+    split_dataGeracao = str(dados_header['dataGeracao']).split('-')
+    data_geracao_validada = f'{split_dataGeracao[2]}/{split_dataGeracao[1]}/{split_dataGeracao[0]}'
+    dados_sb_header = OrderedDict()
+    dados_sb_header["sb:codigoLayout"] = 'DH001'
+    dados_sb_header["sb:dataGeracao"] = data_geracao_validada
+    dados_sb_header["sb:sequencialGeracao"] = dados_header['sequecialGeracao']
+    dados_sb_header["sb:anoReferencia"] = dados_header['anoReferencia']
+    dados_sb_header["sb:ugResponsavel"] = dados_header['ugResponsavel']
+    dados_sb_header["sb:cpfResponsavel"] = dados_header['cpfResponsavel']
+
+    dados_arquivos['sb:header'] = dados_sb_header
     dados_arquivos['sb:detalhes'] = {"sb:detalhe": lista_de_detalhes}
     dados_arquivos['sb:trailler'] = {
                 "sb:quantidadeDetalhe": len(lista_de_detalhes)
--- PDFtoXML/app.py
+++ PDFtoXML/app.py
@@ -25,86 +25,49 @@ dadosGeraisAuxilios = {
 
 
 
-def processDatatoXML(dadosDH: dict, dadosAuxilios: dict):
-    item1 = convertXML(
-        codUgEmit=dadosDH["ugResponsavel"],
-        anoDH=dadosDH["anoReferencia"],
-        codTipoDH='RP',
-        dtEmis=dadosDH["dataGeracao"],
-        dtVenc='2021-12-31',
-        codUgPgto=dadosDH["ugResponsavel"],
-        vlr='300',
-        txtObser=f'{dadosAuxilios["tipoAuxilio"]}-{dadosAuxilios["mesCompetenciaAuxilio"]}/{dadosAuxilios["anoCompetenciaAuxilio"]}-{dadosAuxilios["processoSEI"]}- Abrão Abreu',
-        txtProcesso=dadosAuxilios["processoSEI"],
-        dtAteste=dadosAuxilios['dataAteste'],
-        codCredorDevedor=11111111100,
-        dtPgtoReceb=dadosAuxilios['dataPagamentoPrevista'],
-        codIdentEmit=dadosDH["ugResponsavel"],
-        dtEmisDocOrigem=['dataAteste'],
-        numDocOrigem=dadosAuxilios["mesCompetenciaAuxilio"][:18],
-        numSeqItemPco='1',
-        codSit=dadosDH["codigoSituacaoDH"],
-        codUgEmpe=dadosDH["ugResponsavel"],
-        numEmpe=dadosAuxilios["numeroEmpenho"],
-        codSubItemEmpe='1',
-        numClassA='394110100',
-        numSeqItemCusto='1',
-        codCentroCusto='CC-GENERICO',
-        mesReferencia=dadosAuxilios["mesCompetenciaAuxilio"],
-        anoReferencia=dadosAuxilios["anoCompetenciaAuxilio"],
-        codUgBenef=dadosDH["ugResponsavel"],
-        numSeqPai='1',
-        codTipoOB='OBC',
-        banco='104',
-        agencia='1111',
-        conta='11111111',
-        contaGoverno='UNICA',
-    )
+def processDatatoXML(dadosDH: dict, dadosAuxilios: dict, dadosPagamentoAlunos: list):
+    lista_de_detalhes = []
 
-    item2 = convertXML(
-        codUgEmit=dadosDH["ugResponsavel"],
-        anoDH=dadosDH["anoReferencia"],
-        codTipoDH='RP',
-        dtEmis=dadosDH["dataGeracao"],
-        dtVenc='2021-12-31',
-        codUgPgto=dadosDH["ugResponsavel"],
-        vlr='300',
-        txtObser=f'{dadosAuxilios["tipoAuxilio"]}-{dadosAuxilios["mesCompetenciaAuxilio"]}/{dadosAuxilios["anoCompetenciaAuxilio"]}-{dadosAuxilios["processoSEI"]}- Bernardo Broia',
-        txtProcesso=dadosAuxilios["processoSEI"],
-        dtAteste=dadosAuxilios['dataAteste'],
-        codCredorDevedor=22222222200,
-        dtPgtoReceb=dadosAuxilios['dataPagamentoPrevista'],
-        codIdentEmit=dadosDH["ugResponsavel"],
-        dtEmisDocOrigem=['dataAteste'],
-        numDocOrigem=dadosAuxilios["mesCompetenciaAuxilio"][:18],
-        numSeqItemPco='1',
-        codSit=dadosDH["codigoSituacaoDH"],
-        codUgEmpe=dadosDH["ugResponsavel"],
-        numEmpe=dadosAuxilios["numeroEmpenho"],
-        codSubItemEmpe='1',
-        numClassA='394110100',
-        numSeqItemCusto='1',
-        codCentroCusto='CC-GENERICO',
-        mesReferencia=dadosAuxilios["mesCompetenciaAuxilio"],
-        anoReferencia=dadosAuxilios["anoCompetenciaAuxilio"],
-        codUgBenef=dadosDH["ugResponsavel"],
-        numSeqPai='1',
-        codTipoOB='OBC',
-        banco='104',
-        agencia='2222',
-        conta='22222222',
-        contaGoverno='UNICA',
-    )
+    for dados_aluno in dadosPagamentoAlunos:
+        detalhe_xml_aluno = convertXML(
+            codUgEmit=dadosDH["ugResponsavel"],
+            anoDH=dadosDH["anoReferencia"],
+            codTipoDH='RP',
+            dtEmis=dadosDH["dataGeracao"],
+            dtVenc=dadosAuxilios['dataPagamentoPrevista'],
+            codUgPgto=dadosDH["ugResponsavel"],
+            vlr=dados_aluno[5],
+            txtObser=f'{dadosAuxilios["tipoAuxilio"]} - Mês {dadosAuxilios["mesCompetenciaAuxilio"]}/{dadosAuxilios["anoCompetenciaAuxilio"]} - {dadosAuxilios["processoSEI"]} - {dados_aluno[0][:20]}',
+            txtProcesso=dadosAuxilios["processoSEI"],
+            dtAteste=dadosAuxilios['dataAteste'],
+            codCredorDevedor=dados_aluno[1],
+            dtPgtoReceb=dadosAuxilios['dataPagamentoPrevista'],
+            codIdentEmit=dadosDH["ugResponsavel"],
+            dtEmisDocOrigem=dadosAuxilios['dataAteste'],
+            numDocOrigem=f'{dadosAuxilios["mesCompetenciaAuxilio"]}/{dadosAuxilios["anoCompetenciaAuxilio"]}'[:18],
+            numSeqItemPco='1',
+            codSit=dadosDH["codigoSituacaoDH"],
+            codUgEmpe=dadosDH["ugResponsavel"],
+            numEmpe=dadosAuxilios["numeroEmpenho"],
+            codSubItemEmpe='1',
+            numClassA='394110100',
+            numSeqItemCusto='1',
+            codCentroCusto='CC-GENERICO',
+            mesReferencia=dadosAuxilios["mesCompetenciaAuxilio"],
+            anoReferencia=dadosAuxilios["anoCompetenciaAuxilio"],
+            codUgBenef=dadosDH["ugResponsavel"],
+            numSeqPai='1',
+            codTipoOB='OBC',
+            banco=dados_aluno[2],
+            agencia=dados_aluno[3],
+            conta=dados_aluno[4],
+            contaGoverno='UNICA',
+        )
+        lista_de_detalhes.append(detalhe_xml_aluno.incluirItem())
 
-    testeXML = receberXML(
-        lista_de_detalhes=[item1.incluirItem(),item2.incluirItem()]
-    )
+    arquivoXML = receberXML(lista_de_detalhes,dadosDH)
 
-    st.code(testeXML)
-
-    linkxml = f'<a href="data:file/xml,{quote(testeXML)}" download="lista_pagamentos.xml">Download XML</a>'
-
-    st.markdown(linkxml,unsafe_allow_html=True)
+    return arquivoXML
 
 
 
@@ -146,81 +109,105 @@ if fileUploaded is not None:
         processPdf.cleanDataframe()
 
     with st.expander("Visualise os dados extraídos do PDF processado.", expanded=False):
-        tableFromPDF = st.dataframe(df_data_students)
+        st.dataframe(df_data_students)
     
-    with st.form("formDadosGeraisSIAFI"):
-        st.markdown("### **Dados Gerais do Documento Hábil Siafi**")
-        st.markdown("---")
-        dadosGeraisDH['codigoSituacaoDH'] = st.text_input(
-            "Código da Situação a ser usada na apropriação Documento Hábil",
-            value="DSP061"
-            )
-        dadosGeraisDH['sequecialGeracao'] = st.number_input(
-            "Sequencial de arquivos submetidos no SIAFI pela UG no dia.",
-            min_value=0,
-            value=1,
-            step=1
-        )
-        dadosGeraisDH['anoReferencia'] = st.number_input(
-            "Ano Corrente na data de Apropriação do Documento Hábil",
-            value=2021,
-            step=1,
-            min_value=2021,
-            max_value=2025
-        )
-        dadosGeraisDH['ugResponsavel'] = st.text_input(
-            "UG do operador ao fazer o upload do arquivo no SIAFI",
-            value="158953",
-            max_chars=6
-        )
-        dadosGeraisDH['cpfResponsavel'] = st.text_input(
-            "CPF do operador que fará o upload do arquivo no SIAFI",
-            max_chars=11,
-            value="",
-            help="Apenas números, sem traços ou pontos"
-        )
-        dadosGeraisDH['dataGeracao'] = st.date_input(
-            "Data de upload do arquivo no SIAFI"
-        )
+    with st.expander('Dados Gerais do Documento Hábil',expanded=True):
+        with st.form("formDadosGeraisSIAFI"):
 
-        st.form_submit_button("Confirmar Dados Gerais DH")
+            col1,col2 = st.columns(2)
+            col3,col4 = st.columns(2)
+            col5,col6 = st.columns(2)
+            with col1:
+                dadosGeraisDH['codigoSituacaoDH'] = st.text_input(
+                    "Código da Situação a ser usada na apropriação Documento Hábil",
+                    value="DSP061"
+                    )
+            with col2:
+                dadosGeraisDH['sequecialGeracao'] = st.number_input(
+                    "Sequencial de arquivos submetidos no SIAFI pela UG no dia.",
+                    min_value=0,
+                    value=1,
+                    step=1
+            )
+            with col3:
+                dadosGeraisDH['dataGeracao'] = st.date_input(
+                    "Data de upload do arquivo no SIAFI"
+                )
+            with col4:
+                dadosGeraisDH['anoReferencia'] = st.number_input(
+                    "Ano Corrente na data de Apropriação do Documento Hábil",
+                    value=2021,
+                    step=1,
+                    min_value=2021,
+                    max_value=2025
+                )
+            with col5:
+                dadosGeraisDH['ugResponsavel'] = st.text_input(
+                    "UG do operador ao fazer o upload do arquivo no SIAFI",
+                    value="158953",
+                    max_chars=6
+                )
+            with col6:
+                dadosGeraisDH['cpfResponsavel'] = st.text_input(
+                    "CPF do operador que fará o upload do arquivo no SIAFI",
+                    max_chars=11,
+                    value="",
+                    help="Apenas números, sem traços ou pontos"
+                )
+            submit_dadosGeraisDH = st.form_submit_button("Confirmar Dados Gerais DH")
     
-    with st.form("formDadosMesAuxilioSISAE"):
-        st.markdown("### **Dados Gerais dos Auxilios**")
-        st.markdown("---")
-        dadosGeraisAuxilios['tipoAuxilio'] = st.text_input(
-            "Informe o tipo de auxílio estudantil a ser pago aos alunos",
-            value="Auxílio Emergêncial"
-            )
-        dadosGeraisAuxilios['mesCompetenciaAuxilio'] = st.text_input(
-            "Informe o mês competência dos Auxilios com dois dígitos - Ex.: 06"
-        )
-        dadosGeraisAuxilios['anoCompetenciaAuxilio'] = dadosGeraisDH['anoReferencia']
-        dadosGeraisAuxilios["processoSEI"] = st.text_input(
-            "Informe o número processo no SEI"
-        )
-        dadosGeraisAuxilios["dataAteste"] = st.date_input(
-            "Informe a data de ateste desse processo de pagamento"
-        )
-        dadosGeraisAuxilios['dataPagamentoPrevista'] = st.date_input(
-            "Informe a data de pagamento prevista"
-        )
-        dadosGeraisAuxilios["numeroEmpenho"] = st.text_input(
-            "Informe o número do Empenho que será utilizado na liquidação e pagamentos dos auxílios"
-        )
-        
+    with st.expander('Dados Gerais dos Auxílios',expanded=True):
+        with st.form("formDadosMesAuxilioSISAE"):
+            col1,col2 = st.columns(2)
+            col3,col4 = st.columns(2)
+            col5,col6 = st.columns(2)
+            with col1:
+                dadosGeraisAuxilios['tipoAuxilio'] = st.text_input(
+                    "Informe o tipo de auxílio estudantil a ser pago aos alunos",
+                    value="Auxílio Emergêncial"
+                )
+            with col2:
+                dadosGeraisAuxilios['mesCompetenciaAuxilio'] = st.text_input(
+                    "Informe o mês competência dos Auxilios com dois dígitos - Ex.: 06"
+                )
+            dadosGeraisAuxilios['anoCompetenciaAuxilio'] = dadosGeraisDH['anoReferencia']
+            with col3:
+                dadosGeraisAuxilios["dataAteste"] = st.date_input(
+                    "Informe a data de ateste desse processo de pagamento"
+                )
+            with col4:
+                dadosGeraisAuxilios['dataPagamentoPrevista'] = st.date_input(
+                    "Informe a data de pagamento prevista"
+                )
+            with col5:
+                dadosGeraisAuxilios["processoSEI"] = st.text_input(
+                    "Informe o número processo no SEI"
+                )
+            with col6:
+                dadosGeraisAuxilios["numeroEmpenho"] = st.text_input(
+                    "Informe o número do Empenho que será utilizado na liquidação e pagamentos dos auxílios"
+                )
+            submit_dadosGeraisAuxilios = st.form_submit_button("Confirmar Dados Gerais dos Auxilios")
 
-        st.form_submit_button("Confirmar Dados Gerais dos Auxilios")
 
     if dadosGeraisDH['cpfResponsavel'] != "" and dadosGeraisAuxilios["mesCompetenciaAuxilio"] != "":
         with st.spinner('Processando os dados e convertendo para XML...'):
-            processDatatoXML(
+            arquivoXML = processDatatoXML(
                 dadosGeraisDH,
-                dadosGeraisAuxilios
+                dadosGeraisAuxilios,
+                df_data_students.values.tolist()
             )
+            with st.expander("Visualise os dados extraídos do PDF processado.", expanded=False):
+                st.code(arquivoXML)
+        
+        linkxml = f'<a href="data:file/xml,{quote(arquivoXML)}" download="lista_pagamentos.xml">Download XML</a>'
+
+        st.success('Os dados foram processados com sucesso e o arquivo XML pode ser baixado no botão abaixo')
+        st.markdown(linkxml,unsafe_allow_html=True)
         
     else:
         st.warning("Informe o CPF do responsável e Mẽs de competência para continuar")
+
 
 
     
